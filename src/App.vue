04fccb4823bd0fa4d4513892b92157078cc66a87
Jakkarin Sae-Tiew <jakkarin.sae-tiew@outlook.com>
2019-03-27 00:24:48 -0400

    Home page completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" style="background-color:blueviolet;">
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import './assets/css/main.css'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
+
 <style>
+@import url('https://fonts.googleapis.com/css?family=Quicksand');
+body {
+  background-color: #332A3E;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="home">
+    <header>
+      <div>
+        <img width="330" alt="Aparecium logo" src="../assets/logo.svg">
+        <h1 class="logo-text mt-3 mb-10">Reddit Aparecium</h1>
+
+        <form class="w-max">
+          <div class="w-1/2 flex mx-auto items-center">
+            <input
+              @keyup.enter="fetchData()"
+              v-model="input"
+              type="text"
+              class="input-form flex-1 appearance-none rounded-l-lg bg-grey-light shadow-md p-4 text-grey-darker mr-0 focus:outline-none focus:bg-grey-ligtest"
+              placeholder="Copy Reddit thread URL here..."
+              autocapitalize="off"
+              autocorrect="off"
+            >
+            <button
+              @click="fetchData()"
+              class="input-btn color-3 appearance-none bg-indigo-darker text-grey-light text-base font-semibold tracking-wide uppercase p-4 rounded-r-lg shadow-md hover:bg-indigo-dark focus:outline-none"
+              type="button"
+            >GO</button>
+          </div>
+        </form>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+export default {
+  name: "home",
+  components: {
+    PulseLoader
+  }
+};
+</script>
+
+<style scoped>
+header {
+  position: relative;
+  background: #fb5f2b;
+  padding: 5rem 0 5rem;
+  margin-bottom: 3rem;
+  box-shadow: 0 10px 80px -2px rgba(0, 0, 0, 0.5);
+  overflow: hidden;
+}
+.input-form {
+  font-family: "Quicksand", sans-serif;
+  opacity: 0.75;
+  transition: 0.5s;
+}
+.input-form:focus {
+  opacity: 1;
+  transition: 0.5s;
+}
+* {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+.input-btn {
+  background: #30133d;
+  cursor: pointer;
+  text-align: center;
+  background-size: 300% 100%;
+  moz-transition: all 0.4s ease-in-out;
+  -o-transition: all 0.4s ease-in-out;
+  -webkit-transition: all 0.4s ease-in-out;
+  transition: all 0.4s ease-in-out;
+}
+.input-btn:hover {
+  background-position: 100% 0;
+  moz-transition: all 0.4s ease-in-out;
+  -o-transition: all 0.4s ease-in-out;
+  -webkit-transition: all 0.4s ease-in-out;
+  transition: all 0.4s ease-in-out;
+}
+.input-btn.color-3 {
+    background-image: linear-gradient(to right, #5165be, #51336e, #526da1, rgb(95, 35, 122));
+}
+.input-btn:active {
+  transform: translateY(1px);
+  transition: all 0.1s ease-in-out;
+}
+
+.logo-text {
+  font-family: "Quicksand", sans-serif;
+  font-weight: lighter;
+  color: #f2f2f2;
+}
+</style>
+
