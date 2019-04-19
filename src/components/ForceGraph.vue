d743e0340c56ada1bc73c0074508747ca6cc6367
Jakkarin Sae-Tiew <jakkarin.sae-tiew@outlook.com>
2019-04-19 17:55:49 -0400

    Tested working force layout, performance issue

--- src/components/ForceGraph.vue
+++ src/components/ForceGraph.vue
@@ -1,18 +1,8 @@
 <template>
-  <div class="bg-black text-grey-lighter rounded shadow-md p-2">
-    <svg :height="height" :width="width">
-      <g transform="translate(5,5)">
-        <circle
-          v-for="(comment, i) in arrayData.comments"
-          :key="comment.id"
-          :cx="Math.floor(Math.random() * 500) + width/5"
-          :cy="Math.floor(Math.random() * 500)"
-          :r="comment.LDA_best_topic_score*50"
-          :fill="colors(parseInt(comment.LDA_best_topic))"
-          stroke="none"
-        ></circle>
-      </g>
-    </svg>
+  <div>
+    {{mousePosition}}
+    <button @click="reload">reload</button>
+    <svg :height="height" :width="width" @mousemove="onMouseMove($event)"></svg>
   </div>
 </template>
 
@@ -27,36 +17,109 @@ export default {
       height: 500,
       padding: 1.5, // separation between same-color nodes
       clusterPadding: 6, // separation between different-color nodes
-      maxRadius: 12,
+      nodes: [],
       simulation: null,
-      colors: d3.scaleOrdinal(d3.schemeCategory10)
+      mousePosition: {
+        x: 0,
+        y: 0
+      }
     };
   },
   props: {
     arrayData: Object
   },
+  mounted() {
+    this.loadData(); // initially load json
+  },
   created() {},
-  methods: {},
+  methods: {
+    // load data
+    loadData() {
+      var svg = d3
+        .selectAll("svg")
+        .attr("width", this.width)
+        .attr("height", this.height);
+
+      var colors = d3.scaleOrdinal(d3.schemeCategory10);
+
+      this.nodes = this.arrayData.comments;
+      this.simulation = d3
+        .forceSimulation(this.nodes)
+        .force("charge", d3.forceManyBody().strength(300))
+        .force("center", d3.forceCenter(this.width / 2, this.height / 2))
+        .force("collide", d3.forceCollide(10).strength(1));
+
+      var node = svg
+        .append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(this.nodes)
+        .enter()
+        .append("circle")
+        // .attr("cx", Math.random() * this.width)
+        // .attr("cy", Math.random() * this.height)
+        .attr("r", function(d) {
+          parseInt(d.LDA_best_topic_score * 10);
+        })
+        .attr("fill", function(d) {
+          return colors(parseInt(d.LDA_best_topic));
+        })
+        .call(
+          d3
+            .drag()
+            .on("start", this.dragstarted)
+            .on("drag", this.dragged)
+            .on("end", this.dragended)
+        );
+
+      node.append("title").text(function(d) {
+        return d.id;
+      });
+      console.log("simulation");
+
+      this.simulation.nodes(this.nodes).on("tick", ticked);
+
+      function ticked() {
+        node
+          .attr("cx", function(d) {
+            return d.x;
+          })
+          .attr("cy", function(d) {
+            return d.y;
+          });
+      }
+    },
+    reload() {
+      //console.log('reloading...');
+      this.$svg.empty(); // clear svg --> easiest way to re-create the force graph.
+      this.loadData();
+    },
+    // mouse events
+    onMouseMove(evt) {
+      //console.log(evt, this)
+      this.mousePosition = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+    },
+    // drag event handlers
+    dragstarted(d) {
+      if (!d3.event.active) this.simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    },
+    dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    },
+    dragended(d) {
+      if (!d3.event.active) this.simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+  },
   computed: {
     ...mapGetters(["getarrayData"])
-  },
-  watch: {
-    getarrayData() {
-      console.log("Array Data");
-      console.log(this.getarrayData);
-      console.log("Force Simulation");
-      this.simulation = d3
-        .forceSimulation(this.arrayData.comments)
-        .force("charge", d3.forceManyBody().strength(5))
-        .force("center", d3.forceCenter(this.width / 2, this.height / 2))
-        .force(
-          "collision",
-          d3.forceCollide().radius(function(d) {
-            return d.LDA_best_topic_score * 50;
-          })
-        );
-      this.simulation.restart();
-    }
   }
 };
 </script>
